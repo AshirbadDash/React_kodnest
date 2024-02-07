e7f27852597e23e15c0591a2b7526255de47417c
ashirbad dash <ashirbadash2000@gmail.com>
2024-02-07 16:26:33 +0530

    reactjs_day1

---
+++ app1/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Ritik's First React App he is busy in learning React. 
+          his goal is to become a full stack developer.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React(Trainer Ritik)
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
